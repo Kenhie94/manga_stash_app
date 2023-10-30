c53e3a5e2b5a0bfb2acfd7c798b2ebd7216a8f96
Kenjy <kenhiebusiness@gmail.com>
2023-10-30 15:42:55 -0700

    Finish Footer

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,72 @@
 <template>
-	<div>
-		<div id="app_style_dimension" class="mx-auto">
-			<NavBar />
-		</div>
-		<div id="manga_stash_style">
-			<div id="app_style_dimension" class="mx-auto">
-        <MenuBar />
-			</div>
-		</div>
-	</div>
+  <div id="app">
+    <div class="wrapper">
+      <header class="app_style_dimension mx-auto">
+        <NavBar />
+      </header>
+      <div id="manga_stash_style">
+        <div class="app_style_dimension mx-auto">
+          <MenuBar />
+        </div>
+        <body class="app_style_dimension mx-auto">
+          <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto repellendus ipsa porro odio quam? Fugiat
+            placeat id expedita, dolore consectetur soluta, quaerat amet dolorem voluptate porro perferendis, eligendi
+            eaque illum.</span>
+        </body>
+      </div>
+    </div>
+    <footer>
+      <div class="app_style_dimension mx-auto">
+        <FooterBar />
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue";
+import FooterBar from "./components/FooterBar.vue";
 import MenuBar from "./components/MenuBar.vue";
 
 export default {
-	name: "App",
-	components: {
+  name: "App",
+  components: {
     NavBar,
-    MenuBar
-},
+    MenuBar,
+    FooterBar
+  },
 };
 </script>
 
 <style>
 @import '../public/css/font.css';
 
+body,
+html {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+footer {
+  background-color: #687EFF;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
 #manga_stash_style {
-	background-color: #b9b4c7;
-}
-#app_style_dimension {
-	max-width: 1062px;
+  background-color: #A0E9FF;
 }
 
-#section_style {
-	background-color: #5c5470;
+.app_style_dimension {
+  max-width: 1062px;
 }
 
+.wrapper {
+  flex: 1;
+}
 </style>
---
+++ src/components/FooterBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="footerbar_style" class="d-flex justify-content-between">
+    <div class="d-flex align-items-center">
+      <div class="title_footer">©<span style="color: red">M</span>anga<span style="color: red">S</span>tash</div>
+    </div>
+    <div class="d-flex align-items-center">
+      <span>Links</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterBar',
+  data() {
+    return {
+      // Your data properties here
+    };
+  },
+  methods: {
+    // Your methods here
+  },
+};
+</script>
+
+<style scoped>
+/* Your component-specific styles here */
+#footerbar_style {
+  color: white;
+  font-weight: bold;
+}
+
+.title_footer {
+  font-family: BananaYeti, serif;
+  font-size: 2.5rem;
+}
+</style>
