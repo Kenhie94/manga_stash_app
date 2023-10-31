aad4eda78c7f1964fb7fde510db02d25e949e5a9
Kenjy <kenhiebusiness@gmail.com>
2023-10-31 15:08:02 -0700

    Created routers

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Home from './views/Home.vue';
+import AddManga from './views/AddManga.vue';
+import Community from './views/Community.vue';
+
+const routes = [
+  { path: '/', component: Home},
+  { path: 'Add', component: AddManga},
+  { path: 'Community', component: Community}
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+createApp(App).use(router).mount('#app')
