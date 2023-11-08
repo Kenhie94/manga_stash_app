48c0b7d4c888ac586ab87a533817b84c3d2a0318
Kenjy <kenhiebusiness@gmail.com>
2023-11-07 17:17:26 -0800

    Testing out data mapping

--- Back_End/app.js
+++ Back_End/app.js
@@ -1,35 +1,35 @@
 // Import modules
-const express = require('express')
+const express = require("express");
 const app = express();
 const port = 8080;
-const mongoose = require('mongoose');
-const MangaModelSchema = require('./models/mangaSchema');
-const url = 'mongodb+srv://Kenhie_Manga:Yuru1camp2is3the4best5@mangaapp.87cpygr.mongodb.net/manga_db'
+const dbUrl = "mongodb+srv://Kenhie_Manga:Yuru1camp2is3the4best5@mangaapp.87cpygr.mongodb.net/manga_db";
+const mongoose = require("mongoose");
+const MangaModelSchema = require("./models/mangaSchema");
 
 //Connection to MongoDB Atlas Database
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('Connected to Manga DB');
-})
-.catch ((error) => {
-  console.log('Cannot connect to server:', error)
-});
+mongoose
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to Manga DB");
+  })
+  .catch((error) => {
+    console.log("Cannot connect to server:", error);
+  });
 
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   MangaModelSchema.find({})
-  .then(data => {
-    console.log('Retrieved data!');
-    res.send(`<pre>${JSON.stringify(data, null, 2)}</pre>`)
-  })
-  .catch(error => {
-    console.error('Error retrieving data:', error)
-  })
+    .then((data) => {
+      const englishTitle = data.map(item => item.englishTitle);
+      res.send(`<pre>${JSON.stringify(englishTitle, null, 2)}</pre>`)
+    })
+    .catch((error) => {
+      console.error("Error retrieving data:", error);
+    });
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-})
+});
