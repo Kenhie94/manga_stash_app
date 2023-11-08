f480f6c24e77df964449cc72322632e53785b0b4
KanheiLaptop <kenhiebusiness@gmail.com>
2023-11-08 13:38:00 -0800

    Created Dynamic Pages

---
+++ Front_End/src/views/MangaDetailPage.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<h1>{{	$route.params.id }}</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {},
+	mounted() {
+		console.log('Received ID:', this.$route.params.id)
+	},
+};
+</script>
+
+<style>
+/* Your component's styles */
+</style>
