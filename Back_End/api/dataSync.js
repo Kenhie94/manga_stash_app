cc1fdbd74368654084052ac2bf3bbe5419507c1a
Kenjy <kenhiebusiness@gmail.com>
2023-11-09 16:38:31 -0800

    for loop for multiple entries

--- Back_End/api/dataSync.js
+++ Back_End/api/dataSync.js
@@ -15,9 +15,11 @@ const mangaSchema = new mongoose.Schema({
   title: {
     type: String,
   },
-  altTitles: [{
-    type: Array,
-  }],
+  altTitles: [
+    {
+      type: Array,
+    },
+  ],
   description: {
     type: Array,
   },
@@ -42,43 +44,53 @@ const mangaSchema = new mongoose.Schema({
   contentRating: {
     type: String,
   },
-  tags: [{
-    type: Array,
-  }],
+  tags: [
+    {
+      type: Array,
+    },
+  ],
   links: {
     type: Array,
-  }
+  },
 });
 const MangaDB = mongoose.model("MangaDB", mangaSchema);
 
 // Function to fetch data from an API and store it in your database
 async function fetchDataAndStore() {
-  try {
-    const response = await axios.get("https://api.mangadex.org/manga/1ee97895-4796-4bcf-bcd1-5ef99c011f8b");
-    const apiData = response.data.data; // Assuming the data is in an appropriate format
-    console.log("API DATA:", apiData);
+  const mangaID = [
+    "c52b2ce3-7f95-469c-96b0-479524fb7a1a",
+    "1ee97895-4796-4bcf-bcd1-5ef99c011f8b",
+    "882b1e22-ac83-4613-b8f1-7c48138fa584",
+    "6b958848-c885-4735-9201-12ee77abcb3c",
+  ];
+  for (let i = 0; i < mangaID.length; i++) {
+    try {
+      const response = await axios.get(`https://api.mangadex.org/manga/${mangaID[i]}`);
+      const apiData = response.data.data; // Assuming the data is in an appropriate format
+      console.log("API DATA:", apiData);
 
-    // Process the data and create new documents in your Atlas database
-    const newData = new MangaDB({
-      type: apiData.type,
-      title: apiData.attributes.title.en,
-      altTitles: apiData.attributes.altTitles,
-      description: apiData.attributes.description,
-      originalLanguage: apiData.attributes.originalLanguage,
-      lastVolume: apiData.attributes.lastVolume,
-      lastChapter: apiData.attributes.lastChapter,
-      publicationDemographic: apiData.attributes.publicationDemographic,
-      status: apiData.attributes.status,
-      year: apiData.attributes.year,
-      contentRating: apiData.attributes.contentRating,
-      tags: apiData.attributes.tags,
-      links: apiData.attributes.links,
-    });
+      // Process the data and create new documents in your Atlas database
+      const newData = new MangaDB({
+        type: apiData.type,
+        title: apiData.attributes.title.en,
+        altTitles: apiData.attributes.altTitles,
+        description: apiData.attributes.description,
+        originalLanguage: apiData.attributes.originalLanguage,
+        lastVolume: apiData.attributes.lastVolume,
+        lastChapter: apiData.attributes.lastChapter,
+        publicationDemographic: apiData.attributes.publicationDemographic,
+        status: apiData.attributes.status,
+        year: apiData.attributes.year,
+        contentRating: apiData.attributes.contentRating,
+        tags: apiData.attributes.tags,
+        links: apiData.attributes.links,
+      });
 
-    const savedData = await newData.save(); // Save the new document to your database
-    console.log("Data stored in your database.", savedData);
-  } catch (error) {
-    console.error("Error:", error);
+      const savedData = await newData.save(); // Save the new document to your database
+      console.log("Data stored in your database.", savedData);
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
 }
 
