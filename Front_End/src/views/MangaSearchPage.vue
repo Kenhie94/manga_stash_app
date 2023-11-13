9ba0a7d3dfacc0455d52fc97f16aa06eb0912f4e
Kenjy <kenhiebusiness@gmail.com>
2023-11-13 13:19:22 -0800

    MangaSearchPage Vue

---
+++ Front_End/src/views/MangaSearchPage.vue
@@ -0,0 +1,50 @@
+!<template>
+  <div id="app">
+    <div class="wrapper">
+      <header class="app_style_dimension mx-auto">
+        <NavBar />
+      </header>
+      <div class="app_style_dimension mx-auto">
+        <body>
+          <h1>Multiple Manga Title:</h1>
+          <p>Blah blah blah</p>
+          <p>Blah blah blah</p>
+          <p>Blah blah blah</p>
+        </body>
+      </div>
+    </div>
+    <footer>
+      <div class="app_style_dimension mx-auto">
+        <FooterBar />
+      </div>
+    </footer>
+    <!-- Your Vue template code here -->
+  </div>
+</template>
+
+<script>
+  import NavBar from '@/components/layout/NavBar.vue';
+  import FooterBar from '@/components/layout/FooterBar.vue';
+
+export default {
+  name: 'MangaSearchPage',
+  data() {
+    return {
+      // Your data properties here
+    };
+  },
+  components: {
+    NavBar,
+    FooterBar,
+  },
+  mounted() {
+    const apiUrl = 'https://api'
+  }
+};
+</script>
+
+<style scoped>
+@import '../../public/css/font.css';
+@import '../../public/css/overview_style.css';
+/* Your component-specific styles here */
+</style>
